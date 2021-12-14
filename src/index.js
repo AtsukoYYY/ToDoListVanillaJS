dba7efa716fa672e9e047677c6bf6d65d9fcb2f6
Atsuko Yamada <60997895+AtsukoYYY@users.noreply.github.com>
2021-12-14 20:20:49 +0900

    add delete機能実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  const inputText = document.getElementById("add_text").value;
+  document.getElementById("add_text").value = "";
+
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list_row";
+
+  const p = document.createElement("p");
+  p.className = "list_item";
+  p.innerHTML = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode.parentNode;
+    document.getElementById("incomplete_list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  li.appendChild(div);
+
+  document.getElementById("incomplete_list").appendChild(li);
+};
+
+document
+  .getElementById("add_button")
+  .addEventListener("click", () => onClickAdd());
